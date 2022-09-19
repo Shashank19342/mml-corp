f36b2ac392513c6dc4ac8fdd4a484e4608caa4e6
Shashank19342 <shashankashutosh4@gmail.com>
2022-09-20 01:25:00 +0530

    feat: added Hero section and vision

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,23 @@
+import "./HeroSection.css";
+import hero_vid from "../assets/hero-video.mp4";
+
+function HeroSection(){
+
+
+    return(
+        <div className="hero">
+            <div className="hero-text">
+                <p>MANUFACTURER <span style={{fontSize: 60 + "px",fontWeight: 600}}>EXPORTER &#38;</span> <span style={{fontSize: 80 + "px", fontWeight: 300}}>SUPPLIER</span></p>
+            </div>
+            <div className="hero-vid">
+                <video loop autoPlay muted width="400" height="550">
+                    <source src={hero_vid} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    )
+
+}
+
+export default HeroSection;
---
+++ src/components/VisionMission.jsx
@@ -0,0 +1,32 @@
+import "./VisionMission.css";
+
+function VisionMission(){
+
+    return(
+        <div className="vision">
+            <h1>Vision</h1>
+            <p>Jupyter Industries was founded with the commitment to offer an ever-improving standard of custom 
+                packaging excellence. To accomplish this goal, we have followed three guiding standards.
+            </p>
+            <div className="goals">
+                <div className="goals-card">
+                    <h3>Value in Addition to Price</h3>
+                    <p>Offer a great product at great price. Our goal is to consistently offer the highest packaging at the lowest prices in the industries.</p>
+                </div>
+                <div className="goals-card">
+                    <h3>A Selection that is Second to None</h3>
+                    <p>Offer an extensive array of sizes, lenghts, color and printing options with an equally impressive pricing.</p>
+                </div>
+                <div className="goals-card">
+                    <h3>Genuine Respect for the Customer</h3>
+                    <p>Simply stated, we will treat you as we ourselves would like to be treated. Our passion for outstanding customer service means we are not staisfied until you are satisfied</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default VisionMission;
+
